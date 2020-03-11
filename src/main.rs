bef27b01d9345d058d3edd987b9a73edd2a1a715
SveOls <sverre.d.olsen@live.no>
2020-03-11 23:22:45 +0100

    t

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use minifb::{Key, KeyRepeat, Menu, MenuHandle, Window, WindowOptions};
+use minifb::{Key, KeyRepeat, Menu, MenuHandle, Window, WindowOptions, MouseMode, MouseButton};
 
 use std::{
     collections::{HashMap, LinkedList, VecDeque},
@@ -11,6 +11,10 @@ mod rect;
 
 pub trait Draw: std::fmt::Debug {
     fn draw(&self, img: &mut [u32], width: usize, pos: (usize, usize));
+    fn width(&self) -> usize; 
+    fn height(&self) -> usize;
+    fn contains(&self, pos: (usize, usize)) -> bool;
+    fn get_outline(&self) -> Box<dyn Draw>;
 }
 
 pub struct Image {
@@ -19,7 +23,7 @@ pub struct Image {
     buff: Vec<u32>,
     change: bool,
     open: bool,
-    effects: HashMap<Key, fn(&mut Image)>,
+    effects: HashMap<Key, fn(&mut Image, Option<usize>)>,
     items: LinkedList<(Box<dyn Draw>, (usize, usize), usize)>,
     queue: (VecDeque<usize>, usize),
     selected: Option<usize>,
@@ -76,8 +80,11 @@ impl Image {
 
             let t_buff = &mut Vec::new();
             mem::swap(&mut self.buff, t_buff);
-            for (bitem, pos, _id) in self.items.iter() {
+            for (bitem, pos, id) in self.items.iter() {
                 bitem.draw(t_buff, width, *pos);
+            }
+            if self.selected.is_some() {
+                self.draw_selected(t_buff);
             }
             mem::swap(&mut self.buff, t_buff);
 
@@ -95,13 +102,13 @@ impl Image {
         self.window.get_keys_pressed(repeat)
     }
 
-    pub fn get_effect(&mut self, inp: Key) {
+    pub fn get_effect(&mut self, inp: Key, val: Option<usize>) {
         if let Some(function) = self.effects.get(&inp) {
-            function(self);
+            function(self, val);
         }
     }
 
-    pub fn add_effect(&mut self, inp: Key, fun: fn(&mut Image)) {
+    pub fn add_effect(&mut self, inp: Key, fun: fn(&mut Image, Option<usize>)) {
         self.effects.insert(inp, fun);
     }
 
@@ -131,7 +138,43 @@ impl Image {
                 let (thing, _, _) = temp.pop_front().unwrap();
                 self.items.append(temp);
                 self.change = true;
+                if Some(id) == self.selected {
+                    self.selected = None;
+                }
                 return Some(thing);
+            }
+        }
+        None
+    }
+    fn draw_selected(&mut self, buff: &mut [u32]) {
+        for (item, pos, id) in self.items.iter() {
+            if Some(*id) == self.selected {
+                item.get_outline().draw(buff, self.get_size().0, *pos);
+            }
+        }
+    }
+    pub fn select(&mut self, id: usize) {
+        self.selected = Some(id);
+        self.change = true;
+    }
+    pub fn get_mouse_pos(&self, mode: MouseMode) -> Option<(f32, f32)> {
+        self.window.get_mouse_pos(mode)
+    }
+    pub fn get_mouse_down(&self, button: MouseButton) -> bool {
+        self.window.get_mouse_down(button)
+    }
+    pub fn get_clicked(&self, x: usize, y: usize) -> Option<usize> {
+        let mut dx;
+        let mut dy;
+        for (item, pos, id) in self.items.iter().rev() {
+            dx = if let Some(a) = x.checked_sub(pos.0) {
+                a
+            } else { continue };
+            dy = if let Some(a) = y.checked_sub(pos.1) {
+                a
+            } else { continue };
+            if item.contains((dx, dy)) {
+                return Some(*id);
             }
         }
         None
@@ -141,37 +184,60 @@ impl Image {
 fn main() -> Result<(), Box<dyn Error>> {
     let mut wind = Image::new("tester", 512, 512, WindowOptions::default(), None)?;
 
-    wind.add_effect(Key::Q, |img: &mut Image| img.change_background(0x770000));
-    wind.add_effect(Key::W, |img: &mut Image| img.change_background(0x007700));
-    wind.add_effect(Key::E, |img: &mut Image| img.change_background(0x000077));
-    wind.add_effect(Key::R, |img: &mut Image| img.change_background(0xCCCCCC));
-    wind.add_effect(Key::Escape, |img: &mut Image| img.close());
+    wind.add_effect(Key::Q, |img: &mut Image, _: Option<usize> | img.change_background(0xFF770000));
+    wind.add_effect(Key::W, |img: &mut Image, _: Option<usize> | img.change_background(0xFF007700));
+    wind.add_effect(Key::E, |img: &mut Image, _: Option<usize> | img.change_background(0xFF000077));
+    wind.add_effect(Key::R, |img: &mut Image, _: Option<usize> | img.change_background(0xFFCCCCCC));
+    wind.add_effect(Key::Escape, |img: &mut Image, _: Option<usize> | img.close());
 
-    let test = rect::Rect::new(vec![vec![0x0; 30]; 20]);
+    
+    let test = rect::Rect::from_bmp("./data/tile.bmp")?;
+    let test2 = rect::Rect::new(vec![vec![0xAA00FF00; 30]; 20]);
     let mut a = Vec::new();
-    a.push(wind.add_item(Box::new(test.clone()), (110, 110)));
-    a.push(wind.add_item(Box::new(test.clone()), (10, 110)));
-    a.push(wind.add_item(Box::new(test.clone()), (110, 10)));
-    a.push(wind.add_item(Box::new(test.clone()), (110, 220)));
-    a.push(wind.add_item(Box::new(test.clone()), (220, 110)));
 
-    wind.add_effect(Key::A, |img: &mut Image| mem::drop(img.remove_item(1)));
-    wind.add_effect(Key::S, |img: &mut Image| mem::drop(img.remove_item(2)));
-    wind.add_effect(Key::D, |img: &mut Image| mem::drop(img.remove_item(3)));
-    wind.add_effect(Key::F, |img: &mut Image| mem::drop(img.remove_item(4)));
-    wind.add_effect(Key::G, |img: &mut Image| mem::drop(img.remove_item(5)));
+
+    for j in 1..16 {
+        for i in 0..16 {
+            a.push(wind.add_item(Box::new(test.clone()), (32 * i, 32 * j)));
+        }
+    }
+
+
+
+    wind.add_effect(Key::A, |img: &mut Image, _: Option<usize> | mem::drop(img.remove_item(1)));
+    wind.add_effect(Key::S, |img: &mut Image, _: Option<usize> | mem::drop(img.remove_item(2)));
+    wind.add_effect(Key::D, |img: &mut Image, _: Option<usize> | mem::drop(img.remove_item(3)));
+    wind.add_effect(Key::F, |img: &mut Image, _: Option<usize> | mem::drop(img.remove_item(4)));
+    wind.add_effect(Key::G, |img: &mut Image, _: Option<usize> | mem::drop(img.remove_item(5)));
+    wind.add_effect(Key::Z, |img: &mut Image, _: Option<usize> | img.select(1));
+    wind.add_effect(Key::X, |img: &mut Image, _: Option<usize> | img.select(2));
+    wind.add_effect(Key::C, |img: &mut Image, _: Option<usize> | img.select(3));
+    wind.add_effect(Key::V, |img: &mut Image, _: Option<usize> | img.select(4));
+    wind.add_effect(Key::B, |img: &mut Image, _: Option<usize> | img.select(5));
 
     // wind.remove_item(a[0]);
+    wind.selected = wind.get_clicked(15, 115);
 
     while wind.is_open() {
-        wind.limit_update_rate(Some(Duration::from_millis(10)));
+        wind.limit_update_rate(Some(Duration::from_millis(20)));
         wind.update_with_buffer()?;
 
         if let Some(keys) = wind.get_keys_pressed(KeyRepeat::No) {
             for t in keys {
-                wind.get_effect(t);
+                wind.get_effect(t, None);
             }
         }
+        if wind.get_mouse_down(MouseButton::Left) {
+            if let Some((x, y)) = wind.get_mouse_pos(MouseMode::Clamp) {
+                if let Some(b) = wind.get_clicked(x as usize, y as usize) {
+                    wind.select(b);
+                }
+            }
+        } else if wind.get_mouse_down(MouseButton::Right) {
+            if let Some((x, y)) = wind.get_mouse_pos(MouseMode::Clamp) {
+                // wind.add_item(Box::new(test2.clone()), (x as usize, y as usize));
+            }
+        } 
     }
     Ok(())
 }
--- src/rect.rs
+++ src/rect.rs
@@ -1,3 +1,5 @@
+use bmp;
+
 #[derive(Clone, Debug)]
 pub struct Rect(Vec<Vec<u32>>);
 
@@ -13,11 +15,35 @@ impl Rect {
             temp.push(inp[i]);
             if (i + 1) % w == 0 {
                 ret.push(temp.clone());
-                println!("{:.x?}", temp);
                 temp.clear();
             }
         }
         Rect(ret)
+    }
+    pub fn new_outline(&self) -> Rect {
+        let w = self.0[0].len();
+        let h = self.0.len();
+        let mut ret = Vec::with_capacity(h);
+        ret.push(vec![0xFF000000; w]);
+        let mut temp = vec![0xFF000000];
+        temp.append(&mut vec![0xFFFFFF; w - 2]);
+        temp.push(0xFF000000);
+        for _ in 2..h {
+            ret.push(temp.clone());
+        }
+        ret.push(vec![0xFF000000; w]);
+        Rect(ret)
+    }
+    pub fn from_bmp(name: &str) -> Result<Rect, Box<dyn std::error::Error>> {
+        let a = bmp::open(name)?;
+        let mut ret = Vec::new();
+        let w = a.get_width() as usize;
+        let mut pix: bmp::Pixel;
+        for (x, y) in a.coordinates() {
+            pix = a.get_pixel(x, y);
+            ret.push(0xFF000000 + (pix.r as u32) * 0x10000 + (pix.g as u32) * 0x100 + (pix.b as u32));
+        }
+        Ok(Rect::new_w(ret, w))
     }
 }
 
@@ -25,8 +51,29 @@ impl super::Draw for Rect {
     fn draw(&self, inp: &mut [u32], width: usize, (x, y): (usize, usize)) {
         for i in 0..self.0.len() {
             for j in 0..self.0[0].len() {
-                inp[(i + y) * width + x + j] = self.0[i][j]
+                // implement opacity!!!!
+                if self.0[i][j] / 0x1000000 != 0 {
+                    if let Some(a) = inp.get_mut((i + y) * width + x + j) {
+                        *a = self.0[i][j];
+                    }
+                }
             }
         }
+    }
+    fn width(&self) -> usize {
+        self.0[0].len()
+    }
+    fn height(&self) -> usize {
+        self.0.len()
+    }
+    fn contains(&self, inp: (usize, usize)) -> bool {
+        if inp.0 < self.width() && inp.1 < self.height() {
+            self.0[inp.1][inp.0] / 0x1000000 != 0
+        } else {
+            false
+        }
+    }
+    fn get_outline(&self) -> Box<dyn super::Draw> {
+        Box::new(self.new_outline())
     }
 }
